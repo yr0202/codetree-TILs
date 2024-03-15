1a5267a51fe6c7200612c6b931f3ac87c722f31a
yr0202 <73926438+yr0202@users.noreply.github.com>
2024-03-15 14:48:34 +0900

    Title: 점수 비교, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
+        if(aMath>bMath && aEng>bEng){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+
+    }
+}
