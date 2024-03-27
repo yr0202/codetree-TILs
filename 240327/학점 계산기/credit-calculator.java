587debf10e49797e95d70eae6bf8c2548da745fb
yr0202 <73926438+yr0202@users.noreply.github.com>
2024-03-27 16:35:21 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240327/학점 계산기/credit-calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] grade = new double[n];
+        double sum=0;
+
+        for(int i=0;i<n;i++){
+            grade[i]=sc.nextDouble();
+            sum =+ grade[i];
+        }
+        double average = string.format("%.1f",sum/n);
+        System.out.prinln(average);
+
+        if(average>=4.0) System.out.println("Perfect");
+        else if(average>=3.0) System.out.println("Good");
+        else System.out.println("Poor");
+        
+    }
+}
