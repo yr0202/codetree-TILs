150ed1a04f0732b7616f92d31a654f39e5fb8b49
yr0202 <73926438+yr0202@users.noreply.github.com>
2024-03-27 16:53:12 +0900

    Title: 배열 채우기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240327/배열 채우기/filling-array.java
+++ 240327/배열 채우기/filling-array.java
@@ -5,7 +5,7 @@ public class Main {
         int[] arr = new int[10];
         int i = 0;
 
-        for(i=0; i<n; i++){
+        for(i=0; i<10; i++){
             arr[i] = sc.nextInt();
             if (arr[i]==0) break;
 
